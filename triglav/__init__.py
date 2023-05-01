ccb3c0fe6c5f11d900462ade27b4845c061d254c
Josip Rudar <Josip Rudar>
2023-05-01 12:23:03 -0400

    Fixed imports in __init__.py

--- triglav/__init__.py
+++ triglav/__init__.py
@@ -2,6 +2,6 @@
 This module implements Triglav
 """
 
-from .triglav import Triglav
+from .triglav import Triglav, CLRTransformer, Scaler, NoScale, ETCProx, NoResample
 
-__all__ = ["Triglav"]
+__all__ = ['Triglav', 'CLRTransformer', 'Scaler', 'NoScale', 'ETCProx', 'NoResample']
