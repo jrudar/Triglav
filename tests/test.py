beb317e6d2b5a5013e5d7deac4d8b976f62d66b1
Josip Rudar <Josip Rudar>
2023-05-06 18:07:02 -0400

    Uploading tests

---
+++ tests/test.py
@@ -0,0 +1,56 @@
+
+from triglav import Triglav, ETCProx
+
+from sklearn.datasets import make_classification
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+import pandas as pd
+
+import numpy as np
+
+from pathlib import Path
+
+if __name__ == "__main__":
+
+    dirpath = Path(__file__).parent
+
+    expected_output = dirpath / 'data/expected_output.csv'
+
+    #Create the dataset
+    X, y = make_classification(n_samples = 200,
+                               n_features = 20,
+                               n_informative = 5,
+                               n_redundant = 2,
+                               n_repeated = 0,
+                               n_classes = 2,
+                               shuffle = False,
+                               random_state = 0)
+
+    #Split into train and test sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, 
+                                                        test_size = 0.2, 
+                                                        random_state = 0, 
+                                                        stratify = y)
+
+    #Set up Triglav
+    model = Triglav(n_jobs = 10,
+                    verbose = 3,
+                    metric = "euclidean",
+                    linkage = "ward", 
+                    criterion="maxclust",
+                    thresh = 9,
+                    transformer=StandardScaler())
+
+    #Identify predictive features
+    model.fit(X_train, y_train)
+
+    features_selected = model.selected_
+    features_best = model.selected_best_
+
+    df = pd.DataFrame(data = [features_best, features_selected], index = ["Selected Best", "Selected"], columns = [i for i in range(0, 20)])
+    
+    test_df = pd.read_csv(expected_output)
+
+    pd.assert_frame_equal(df, df_expected, check_dtype = False)
+
