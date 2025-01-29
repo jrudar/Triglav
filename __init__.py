7de2df77c137dc9f38900f8ed4f2d63a3e8b8c02
jrudar <37217429+jrudar@users.noreply.github.com>
2025-01-29 16:56:43 -0500

    Delete __init__.py

--- __init__.py
+++
@@ -1,7 +0,0 @@
-"""
-This module implements Triglav
-"""
-
-from .triglav import Triglav, Scaler, NoScale, ETCProx, NoResample, get_shadow
-
-__all__ = ['Triglav', 'Scaler', 'NoScale', 'ETCProx', 'NoResample', 'get_shadow']
